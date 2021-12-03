cbcad4f0abb98b8230b0b880a6faf7e87d38a3c7
Camil Grabowski <camil.grabowski@uconn.edu>
2021-12-03 10:01:55 -0500

    added typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,8 +5,43 @@ const typeDefs = gql `
         me: User
     }
 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    input BookInput {
+        bookId: String
+        authors: [String]
+        title: String
+        description: String
+        image: String
+        link: String
+
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Mutation {
         login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: String!): User
     }
 
 `;
